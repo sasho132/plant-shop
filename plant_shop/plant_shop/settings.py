958f2b4f4e22adef449f8b7188c73ce25f200add
Aleksandar Petrov <sasho1320@gmail.com>
2022-12-10 15:37:12 +0200

    Update docker-compose.yml, nginx conf and settings.py

--- plant_shop/plant_shop/settings.py
+++ plant_shop/plant_shop/settings.py
@@ -98,6 +98,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
+STATIC_ROOT = '/tmp/plant_shop/static'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
